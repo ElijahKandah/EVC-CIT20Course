14fd5875f85f191c673f8a830cc30ab6ddba5266
ElijahK <85915872+ElijahKandah@users.noreply.github.com>
2021-06-22 09:15:25 -0700

    updated version number
    added version identifier

--- Source/Helloworld.py
+++ Source/Helloworld.py
@@ -1,12 +1,5 @@
-'''
-    This is the hello world program in python
-'''
-import sys
+#
+# this is hello world 
+#
 
-# print the version of python you are running, it should be 3.x
-print()
-print("Version is: ", sys.version)
-print()
-
-# print hello world
-print("Hello World ")
+print("hello world")
