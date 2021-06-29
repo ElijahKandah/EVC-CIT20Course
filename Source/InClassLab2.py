da1a2a881bf7d04f55f3b52d0d7f7320dcbe0555
ElijahK <85915872+ElijahKandah@users.noreply.github.com>
2021-06-29 11:47:32 -0700

    added basic menu system
    implemented basic menu

---
+++ Source/InClassLab2.py
@@ -0,0 +1,13 @@
+# Version Number check
+import sys
+print("Version is: ", sys.version)
+
+# name check
+fname = str("Elijah")
+lname = str("Kandah")
+print(fname,lname)
+
+# multiplication check 
+x=2
+y=1.5
+print(x*y)
